b580db8d00023adcb01b21cb203e681e272bcd56
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-02-23 13:28:54 -0800

    refactoring code

--- src/openrave_bridge/pr2model.py
+++ src/openrave_bridge/pr2model.py
@@ -9,53 +9,8 @@ from tf import transformations
 import pr2_control_utilities
 from geometry_msgs.msg import PoseStamped
 import numpy as np
-from tabletop_object_detector.srv import TabletopDetectionResponse
 
-def transform_relative_pose_for_ik(manip, matrix4, ref_frame, targ_frame):
-    robot = manip.GetRobot()
-
-    if ref_frame == "world":
-        worldFromRef = np.eye(4)
-    else:
-        ref = robot.GetLink(ref_frame)
-        worldFromRef = ref.GetTransform()
-
-    if targ_frame == "end_effector":
-        targFromEE = np.eye(4)
-    else:
-        targ = robot.GetLink(targ_frame)
-        worldFromTarg = targ.GetTransform()
-        worldFromEE = manip.GetEndEffectorTransform()
-        targFromEE = np.dot(np.linalg.inv(worldFromTarg), worldFromEE)
-
-    refFromTarg_new = matrix4
-    worldFromEE_new = np.dot(np.dot(worldFromRef, refFromTarg_new), targFromEE)
-
-    return worldFromEE_new
-
-def tf_for_link(T_w_link, manip, link_name):
-    """
-    Transforms an arbitrary link attached to the manipulator
-    e.g. you might want ik for pr2 "r_gripper_tool_frame" instead of the openrave EE frame
-    T_w_link: 4x4 matrix. "world frame from link frame"
-    manip: openrave Manipulator
-    link_name: (you know)
-    filter_options: see openravepy.IkFilterOptions
-    """
-        
-    robot = manip.GetRobot()
-
-    link = robot.GetLink(link_name)
-
-    if not robot.DoesAffect(manip.GetArmJoints()[-1], link.GetIndex()):
-        raise Exception("link %s is not attached to end effector of manipulator %s"%(link_name, manip.GetName()))
-
-    Tcur_w_link = link.GetTransform()
-    Tcur_w_ee = manip.GetEndEffectorTransform()
-    Tf_link_ee = np.linalg.solve(Tcur_w_link, Tcur_w_ee)
-    
-    T_w_ee = T_w_link.dot(Tf_link_ee)
-    return T_w_ee
+import utils
 
 class PR2Robot(object):
     def __init__(self):
@@ -64,14 +19,14 @@ class PR2Robot(object):
         self.env.Add(self.robot)
         
         rospy.loginfo("Loading IK for rightarm")
-        manip = self.robot.SetActiveManipulator("rightarm")
+        self.robot.SetActiveManipulator("rightarm")
         self.rightarm_ik = InverseKinematicsModel(self.robot, 
                                                   iktype=openravepy.IkParameterization.Type.Transform6D)
         if not self.rightarm_ik.load():
             self.rightarm_ik.autogenerate()
             
         rospy.loginfo("Loading IK for leftarm")
-        manip = self.robot.SetActiveManipulator("leftarm")
+        self.robot.SetActiveManipulator("leftarm")
         self.leftarm_ik = InverseKinematicsModel(self.robot, 
                                                  iktype=openravepy.IkParameterization.Type.Transform6D)
         if not self.leftarm_ik.load():
@@ -90,7 +45,6 @@ class PR2Robot(object):
         inds_ros2rave = np.array([self.robot.GetJointIndex(name) for name in ros_names])
         self.good_ros_inds = np.flatnonzero(inds_ros2rave != -1) # ros joints inds with matching rave joint
         self.rave_inds = inds_ros2rave[self.good_ros_inds] # openrave indices corresponding to those joints        
-        
     
     def convertPose(self, pose):
         assert isinstance(pose, PoseStamped)
@@ -102,12 +56,11 @@ class PR2Robot(object):
         
         matrix4 = np.dot(translation, orientation).squeeze()
         return matrix4
-        
     
     def __ik_solution(self, pose, manip, end_effector_link, ignore_end_effector=True):
         T = self.convertPose(pose)
         self.update_rave()
-        worldFromEE = tf_for_link(T, manip, end_effector_link)
+        worldFromEE = utils.tf_for_link(T, manip, end_effector_link)
         filter_options = openravepy.IkFilterOptions.CheckEnvCollisions
         if ignore_end_effector:
             filter_options = filter_options | openravepy.IkFilterOptions.IgnoreEndEffectorCollisions
@@ -147,21 +100,3 @@ class PR2Robot(object):
         self.robot.SetJointValues(rave_values[:20],self.rave_inds[:20])
         self.robot.SetJointValues(rave_values[20:],self.rave_inds[20:])
 
-def add_table(table_msg, env):
-    assert isinstance(table_msg, TabletopDetectionResponse)
-    body = openravepy.RaveCreateKinBody(env,'')
-    body.SetName('table')
-    z = table_msg.detection.table.pose.pose.position.z
-    x_min = table_msg.detection.table.x_min
-    x_max = table_msg.detection.table.x_max
-    y_min = table_msg.detection.table.y_min
-    y_max = table_msg.detection.table.y_max
-    
-    x = (x_max-x_min)/2 + x_min
-    y = (y_max-y_min)/2 + y_min
-    dim_x = (x_max - x_min)/2 + 0.1
-    dim_y = (y_max - y_min)/2 + 0.1
-    body.InitFromBoxes(np.array([[x, y, z, dim_x, dim_y, 0.06]]), True)
-    env.Add(body, True)    
-    
-    return body
